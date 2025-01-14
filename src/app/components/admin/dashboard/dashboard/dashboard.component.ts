c13fa4b44acbbb0c1078323ae1a7a89112be2022
maticandella <matiascandella94@gmail.com>
2025-01-14 14:05:24 -0300

    dashboard incorporado

--- src/app/components/admin/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Author } from '../../../../interfaces/Author';
+import { AuthorService } from '../../../../services/authors.service';
+import { CommonModule } from '@angular/common';
+import { Book } from '../../../../interfaces/Book';
+import { BookService } from '../../../../services/books.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  authors: Author[] = [];
+  books: Book[] = [];
 
+  private authorService = inject(AuthorService);
+  private bookService = inject(BookService);
+
+  ngOnInit(): void {
+    this.getLastAuthors(1, 5);
+    this.getLastBooks(1, 5);
+  }
+
+  getLastAuthors(page: number, limit: number): void {
+    this.authorService.getAll(page, limit).subscribe(response => {
+      this.authors = response.data.items;
+    });
+  }
+
+  getLastBooks(page: number, limit: number): void {
+    this.bookService.getAll(page, limit).subscribe(response => {
+      this.books = response.data.items;
+    });
+  }
 }

15c155d1700e6ebc25bc1c4307950f839963f9d8
maticandella <matiascandella94@gmail.com>
2025-01-21 16:09:01 -0300

    footer incorporado

--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from "../../components/footer/footer/footer.component";
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css'
 })

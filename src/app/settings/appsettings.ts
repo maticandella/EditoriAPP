ca6801a79220a1548239635fde1fac7b670e3154
maticandella <matiascandella94@gmail.com>
2025-02-08 16:08:14 -0300

    ajuste appsettings

--- src/app/settings/appsettings.ts
+++ src/app/settings/appsettings.ts
@@ -1,5 +1,7 @@
+import { environment } from '../../environments/environment';
+
 export const appsettings = {
-    adminUrl: "http://localhost:3000/admin",
-    apiUrl: "http://localhost:3000/api",
-    swaggerUrl: "http://localhost:3000/api-docs"
-}
+  adminUrl: environment.adminUrl,
+  apiUrl: environment.apiUrl,
+  swaggerUrl: environment.swaggerUrl
+};

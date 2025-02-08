88b88ef717d795b13018d97d0ffd2d92f19ba07f
maticandella <matiascandella94@gmail.com>
2025-02-08 15:41:21 -0300

    ajustes de environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    adminUrl: "https://editorial-api.vercel.app/admin",
+    apiUrl: "https://editorial-api.vercel.app/api",
+    swaggerUrl: "https://editorial-api.vercel.app/api/docs"
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    adminUrl: "http://localhost:3000/admin",
+    apiUrl: "http://localhost:3000/api",
+    swaggerUrl: "http://localhost:3000/api/docs"
+};
